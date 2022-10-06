15e3303deff56be43f7a343e05ddde5fe28ff02e
unknown <markgiliadov@gmail.com>
2022-10-06 10:11:04 -0700

    integrating ClickFilter method with MySQL

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ let con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "123123",
-  database: "ig_clone",
+  database: "ig_clone_db",
 });
 
 con.connect((err) => {
@@ -16,7 +16,7 @@ con.connect((err) => {
   console.log("Connected to DB!");
 });
 
-app.get("/", async (req, res) => {
+app.get("/sort", async (req, res) => {
   console.log(req.headers);
   if (req.headers.tablename) {
     const tableName = req.headers.tablename;
@@ -34,6 +34,41 @@ app.get("/", async (req, res) => {
     });
     // }
   }
+});
+
+app.get("/filter", async (req, res) => {
+  console.log(req.headers);
+  // if (req.headers.tablename) {
+    const tableName = req.headers.tablename;
+    const sorting = req.headers.sorting;
+    const orderVal = req.headers.orderval;
+    const firstDate = req.headers.firstdate;
+    const lastDate = req.headers.lastdate;
+    const colFlag = req.headers.colflag;
+    console.log(colFlag)
+    let sqlSelectQuery = ""
+    if(colFlag){
+      console.log('inside')
+      sqlSelectQuery = `SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+      WHERE TABLE_NAME = N'${}' AND TABLE_SCHEMA = "ig_clone_db";`
+
+    }
+    else{
+    console.log(firstDate, lastDate)
+    // let sqlSelectQuery = "";
+    // if (tableName && sorting && orderVal) {
+    console.log("hello");
+    sqlSelectQuery = `SELECT * FROM ${tableName} WHERE created_at BETWEEN ${firstDate.toString()} AND ${lastDate.toString()}`;
+
+    // if (req.headers.sorting == "asc")
+    
+  }
+  con.query(sqlSelectQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+    // }
+  // }
 });
 
 app.listen(3400, () => {
